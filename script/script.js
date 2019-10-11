8a09caa57d83509401e18090246acefa2d0ccdba
IharTsykala <sudja09@bk.ru>
2019-10-11 18:33:57 +0300

    feat: add toggle between the window notification and the picture

--- script/script.js
+++ script/script.js
@@ -4,6 +4,14 @@ picture.src = "assets/img/AllDays.jpg";
 picture.alt = "days of the week";
 showCurrentDay.append(picture);
 
+const notificationContainer = document.getElementById("notificationContainer");
+const mainDescription = document.getElementById("mainDescription");
+const closeNotification = document.getElementById("closeNotification");
+const enter = document.getElementById("notificationContainerEnter");
+const dots = document.getElementsByClassName("notification-container__dots");
+const nameDay = document.getElementById("nameDay");
+const quickNavigation = document.getElementById("quickNavigation");
+
 const week = [
   "Monday",
   "Tuesday",
@@ -16,99 +24,127 @@ const week = [
 
 let currentDay = 2;
 
-const notificationContainer = document.getElementById("notificationContainer");
 setTimeout(() => {
+  // const laughNotificationContainer = () => {
   picture.style.display = "none";
   notificationContainer.style.display = "flex";
-}, 2000);
 
-const closeNotification = document.getElementById("closeNotification");
-closeNotification.addEventListener("click", () => {
-  notificationContainer.style.display = "none";
-  picture.style.display = "block";
-  // if (checkbox_id.checked) {
-  //   localStorage.setItem("disableTips", "hidden");
-  // }
-});
-
-const enter = document.getElementById("notificationContainerEnter");
-enter.addEventListener("click", () => {
-  picture.src = `assets/img/${week[currentDay]}.jpg`;
-  notificationContainer.style.display = "none";
-  picture.style.display = "block";
-});
-
-const dots = document.getElementsByClassName("notification-container__dots");
-const nameDay = document.getElementById("nameDay");
-
-const showDay = n => {
-  currentDay = n;
-
-  if (n > week.length - 1) {
-    currentDay = 0;
-  }
-  if (n < 0) {
-    currentDay = week.length - 1;
-  }
-  nameDay.innerHTML = week[currentDay];
-
-  for (var i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[currentDay].classList.add("active");
-};
-
-const quickNavigation = document.getElementById("quickNavigation");
-
-const setActiveDots = e => {
-  for (let i = 0; i < quickNavigation.children.length; i++) {
-    if (quickNavigation.children[i] === e.target) {
-      for (let i = 0; i < quickNavigation.children.length; i++) {
-        quickNavigation.children[i].classList.remove("active");
-      }
-      e.target.classList.add("active");
-    }
-  }
-};
-
-quickNavigation.addEventListener("click", e => {
-  setActiveDots(e);
-});
-
-const getNextDay = n => {
-  showDay((currentDay += n));
-};
-
-const defineCurrentDay = n => {
-  showDay((currentDay = n));
-};
-
-showDay(currentDay);
-
-const checkKeyPress = key => {
-  if (key.keyCode == "37") {
-    getNextDay(-1);
-  }
-
-  if (key.keyCode == "39") {
-    getNextDay(+1);
-  }
-
-  if (event.code == "Escape") {
+  closeNotification.addEventListener("click", () => {
     notificationContainer.style.display = "none";
-  }
-};
+    picture.style.display = "block";
+    mainDescription.children[0].style.display = "none";
+    mainDescription.children[1].style.display = "flex";
+    // clearTimeout(timerId);
+    // if (checkbox_id.checked) {
+    //   localStorage.setItem("disableTips", "hidden");
+    // }
+  });
 
-// const notificationContainer = document.getElementById("notificationContainer");
+  enter.addEventListener("click", () => {
+    picture.src = `assets/img/${week[currentDay]}.jpg`;
+    notificationContainer.style.display = "none";
+    picture.style.display = "block";
+    mainDescription.children[0].style.display = "none";
+    mainDescription.children[1].style.display = "flex";
+    // clearTimeout(timerId);
+  });
 
-window.document.addEventListener("keydown", key => {
-  if (notificationContainer.style.display === "flex") {
-    checkKeyPress(key);
-  }
-});
+  const showDay = n => {
+    if (n) {
+      currentDay = n;
+    }
 
-// console.log(showCurrentDay);
-// console.log(picture);
+    if (currentDay > week.length - 1) {
+      currentDay = 0;
+    }
+    if (currentDay < 0) {
+      currentDay = week.length - 1;
+    }
+    nameDay.innerHTML = week[currentDay];
+
+    for (var i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[currentDay].classList.add("active");
+  };
+
+  showDay(currentDay);
+
+  const setActiveDots = e => {
+    for (let i = 0; i < quickNavigation.children.length; i++) {
+      if (quickNavigation.children[i] === e.target) {
+        for (let i = 0; i < quickNavigation.children.length; i++) {
+          quickNavigation.children[i].classList.remove("active");
+        }
+        e.target.classList.add("active");
+      }
+    }
+  };
+
+  quickNavigation.addEventListener("click", e => {
+    setActiveDots(e);
+  });
+
+  const getNextDay = n => {
+    showDay((currentDay += n));
+  };
+
+  const prev = document.getElementById("prev");
+  const next = document.getElementById("next");
+
+  prev.addEventListener("click", () => {
+    getNextDay(-1);
+  });
+
+  next.addEventListener("click", () => {
+    getNextDay(1);
+  });
+
+  const defineCurrentDay = n => {
+    showDay((currentDay = n));
+  };
+
+  quickNavigation.addEventListener("click", e => {
+    for (let i = 0; i < quickNavigation.children.length; i++) {
+      if (quickNavigation.children[i] === e.target) {
+        defineCurrentDay(i);
+      }
+    }
+  });
+
+  const checkKeyPress = key => {
+    if (key.keyCode == "37") {
+      getNextDay(-1);
+    }
+
+    if (key.keyCode == "39") {
+      getNextDay(+1);
+    }
+
+    if (event.code == "Escape") {
+      notificationContainer.style.display = "none";
+    }
+  };
+
+  window.document.addEventListener("keydown", key => {
+    if (notificationContainer.style.display === "flex") {
+      checkKeyPress(key);
+    }
+  });
+
+  // };
+
+  // const timerId = setTimeout(() => {
+  // laughNotificationContainer();
+  // }, 2000);
+
+  picture.addEventListener("click", () => {
+    notificationContainer.style.display = "flex";
+    picture.style.display = "none";
+    mainDescription.children[0].style.display = "block";
+    mainDescription.children[1].style.display = "none";
+  });
+}, 2000);
 
 // window.onload = function() {
 //   const notificationClosed = document.getElementById("notification_container");
