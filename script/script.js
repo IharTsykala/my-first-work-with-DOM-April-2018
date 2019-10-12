18eb35f7de9f9c0b4b65546d5abda70123f2e485
IharTsykala <sudja09@bk.ru>
2019-10-12 22:27:15 +0300

    feat: add hot keys

--- script/script.js
+++ script/script.js
@@ -25,28 +25,29 @@ const week = [
 let currentDay = 2;
 
 setTimeout(() => {
-  // const laughNotificationContainer = () => {
   picture.style.display = "none";
   notificationContainer.style.display = "flex";
 
-  closeNotification.addEventListener("click", () => {
+  const close = flag => {
+    if (!flag) {
+      picture.src = `assets/img/${week[currentDay]}.jpg`;
+    }
     notificationContainer.style.display = "none";
     picture.style.display = "block";
     mainDescription.children[0].style.display = "none";
     mainDescription.children[1].style.display = "flex";
-    // clearTimeout(timerId);
+    // const disableTips = localStorage.getItem("disableTips");
     // if (checkbox_id.checked) {
     //   localStorage.setItem("disableTips", "hidden");
     // }
+  };
+
+  closeNotification.addEventListener("click", () => {
+    close(true);
   });
 
   enter.addEventListener("click", () => {
-    picture.src = `assets/img/${week[currentDay]}.jpg`;
-    notificationContainer.style.display = "none";
-    picture.style.display = "block";
-    mainDescription.children[0].style.display = "none";
-    mainDescription.children[1].style.display = "flex";
-    // clearTimeout(timerId);
+    close();
   });
 
   const showDay = n => {
@@ -112,121 +113,41 @@ setTimeout(() => {
     }
   });
 
-  const checkKeyPress = key => {
-    if (key.keyCode == "37") {
-      getNextDay(-1);
-    }
-
-    if (key.keyCode == "39") {
-      getNextDay(+1);
-    }
-
-    if (event.code == "Escape") {
-      notificationContainer.style.display = "none";
-    }
-  };
-
-  window.document.addEventListener("keydown", key => {
-    if (notificationContainer.style.display === "flex") {
-      checkKeyPress(key);
-    }
-  });
-
-  // };
-
-  // const timerId = setTimeout(() => {
-  // laughNotificationContainer();
-  // }, 2000);
-
-  picture.addEventListener("click", () => {
+  const showNotification = () => {
     notificationContainer.style.display = "flex";
     picture.style.display = "none";
     mainDescription.children[0].style.display = "block";
     mainDescription.children[1].style.display = "none";
+  };
+
+  const checkKeyPress = () => {
+    if (event.code === "ArrowLeft") {
+      getNextDay(-1);
+    }
+
+    if (event.code === "ArrowRight") {
+      getNextDay(+1);
+    }
+
+    if (event.code === "Escape") {
+      close(true);
+    }
+
+    if (event.code === "Enter") {
+      close();
+    }
+  };
+
+  window.document.addEventListener("keydown", () => {
+    if (notificationContainer.style.display === "flex") {
+      checkKeyPress();
+    } else {
+      console.log("1");
+      showNotification();
+    }
+  });
+
+  picture.addEventListener("click", () => {
+    showNotification();
   });
 }, 2000);
-
-// window.onload = function() {
-//   const notificationClosed = document.getElementById("notification_container");
-//   const checkbox_id = document.getElementById("checkbox_id");
-//   const nav_dots = document.getElementById("navdots");
-//   const message = document.getElementById("tip");
-
-//   // const disableTips = localStorage.getItem("disableTips");
-
-//   // if (!disableTips) {
-//   //   notificationClosed.style.display = "none";
-//   // } else {
-//   notificationClosed.style.display = "none";
-//   setTimeout(function() {
-//     notificationClosed.style.display = "block";
-//   }, 2000);
-//   // }
-
-//   const closeButton = document.getElementById("closeButton");
-//   closeButton.addEventListener("click", function(e) {
-//     e.preventDefault();
-//     notificationClosed.style.display = "none";
-//     if (checkbox_id.checked) {
-//       localStorage.setItem("disableTips", "hidden");
-//     }
-//   });
-// };
-
-// const notificationClosed = document.getElementById("notification_container");
-
-// const dayTips = ["one", "two", "three", "four", "five", "six", "seven"];
-
-// var slideIndex = 3;
-
-// showContent(slideIndex);
-
-// function plusIndex(n) {
-//   showContent((slideIndex += n));
-// }
-
-// function currentSlide(n) {
-//   showContent((slideIndex = n));
-// }
-
-// function showContent(n) {
-//   var dots = document.getElementsByClassName("dots");
-
-//   if (n > dayTips.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = dayTips.length;
-//   }
-//   document.getElementById("tip").innerHTML = dayTips[slideIndex - 1];
-
-//   for (var i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-// }
-
-// window.document.addEventListener("keydown", checkKeyPress);
-
-// function checkKeyPress(key) {
-//   if (key.keyCode == "37") {
-//     plusIndex(-1);
-//   }
-
-//   if (key.keyCode == "39") {
-//     plusIndex(+1);
-//   }
-
-//   if (event.code == "Escape") {
-//     notificationClosed.style.display = "none";
-//   }
-// }
-
-// var d=document.createElement('div');
-// d.style.width='400px';
-// d.style.height='200px';
-// d.style.background='yellow';
-// d.className='element';
-// document.body.appendChild(d);
-// };
-//  document.getElementById('notification_container').hidden= 1;
-// setTimeout (element, 5000)
