6454cdb59308adb28f06425154bb5185b2594b80
IharTsykala <sudja09@bk.ru>
2019-11-04 23:31:30 +0300

    feat: @media max 1200px

--- script/script.js
+++ script/script.js
@@ -30,151 +30,151 @@ if (localStorage.currentDay) {
   currentDay = +localStorage.currentDay;
 }
 
-setTimeout(() => {
-  bigPicture.style.display = "none";
-  notificationContainer.style.display = "flex";
+// setTimeout(() => {
+//   bigPicture.style.display = "none";
+//   notificationContainer.style.display = "flex";
 
-  checkbox_id.addEventListener("click", () => {
-    if (checkbox_id.checked) {
-      localStorage.setItem("currentDay", currentDay);
-    }
-  });
+//   checkbox_id.addEventListener("click", () => {
+//     if (checkbox_id.checked) {
+//       localStorage.setItem("currentDay", currentDay);
+//     }
+//   });
 
-  const close = flag => {
-    if (!flag) {
-      bigPicture.src = `${week[currentDay].url}`;
-    }
-    notificationContainer.style.display = "none";
-    bigPicture.style.display = "block";
-    mainDescription.children[0].style.display = "none";
-    mainDescription.children[1].style.display = "flex";
-    if (localStorage.length) {
-      windowStorage = localStorage.getItem("window");
-    }
-    if (checkbox_id.checked) {
-      localStorage.setItem("currentDay", currentDay);
-    } else {
-      localStorage.clear();
-    }
-  };
+//   const close = flag => {
+//     if (!flag) {
+//       bigPicture.src = `${week[currentDay].url}`;
+//     }
+//     notificationContainer.style.display = "none";
+//     bigPicture.style.display = "block";
+//     mainDescription.children[0].style.display = "none";
+//     mainDescription.children[1].style.display = "flex";
+//     if (localStorage.length) {
+//       windowStorage = localStorage.getItem("window");
+//     }
+//     if (checkbox_id.checked) {
+//       localStorage.setItem("currentDay", currentDay);
+//     } else {
+//       localStorage.clear();
+//     }
+//   };
 
-  closeNotification.addEventListener("click", () => {
-    close(true);
-  });
+//   closeNotification.addEventListener("click", () => {
+//     close(true);
+//   });
 
-  enter.addEventListener("click", () => {
-    close();
-  });
+//   enter.addEventListener("click", () => {
+//     close();
+//   });
 
-  smallPicture.addEventListener("click", () => {
-    close();
-  });
+//   smallPicture.addEventListener("click", () => {
+//     close();
+//   });
 
-  const showDay = n => {
-    if (checkbox_id.checked) {
-      localStorage.setItem("currentDay", currentDay);
-    } else {
-      localStorage.clear();
-    }
+//   const showDay = n => {
+//     if (checkbox_id.checked) {
+//       localStorage.setItem("currentDay", currentDay);
+//     } else {
+//       localStorage.clear();
+//     }
 
-    if (n) {
-      currentDay = n;
-    }
+//     if (n) {
+//       currentDay = n;
+//     }
 
-    if (currentDay > week.length - 1) {
-      currentDay = 0;
-    }
-    if (currentDay < 0) {
-      currentDay = week.length - 1;
-    }
-    nameDay.innerHTML = `${week[currentDay].day}`;
-    smallPicture.src = `${week[currentDay].url}`;
-    smallPicture.alt = `${week[currentDay].day}`;
+//     if (currentDay > week.length - 1) {
+//       currentDay = 0;
+//     }
+//     if (currentDay < 0) {
+//       currentDay = week.length - 1;
+//     }
+//     nameDay.innerHTML = `${week[currentDay].day}`;
+//     smallPicture.src = `${week[currentDay].url}`;
+//     smallPicture.alt = `${week[currentDay].day}`;
 
-    for (var i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    dots[currentDay].classList.add("active");
-  };
+//     for (var i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     dots[currentDay].classList.add("active");
+//   };
 
-  showDay(currentDay);
+//   showDay(currentDay);
 
-  const setActiveDots = e => {
-    for (let i = 0; i < quickNavigation.children.length; i++) {
-      if (quickNavigation.children[i] === e.target) {
-        for (let i = 0; i < quickNavigation.children.length; i++) {
-          quickNavigation.children[i].classList.remove("active");
-        }
-        e.target.classList.add("active");
-      }
-    }
-  };
+//   const setActiveDots = e => {
+//     for (let i = 0; i < quickNavigation.children.length; i++) {
+//       if (quickNavigation.children[i] === e.target) {
+//         for (let i = 0; i < quickNavigation.children.length; i++) {
+//           quickNavigation.children[i].classList.remove("active");
+//         }
+//         e.target.classList.add("active");
+//       }
+//     }
+//   };
 
-  quickNavigation.addEventListener("click", e => {
-    setActiveDots(e);
-  });
+//   quickNavigation.addEventListener("click", e => {
+//     setActiveDots(e);
+//   });
 
-  const getNextDay = n => {
-    showDay((currentDay += n));
-  };
+//   const getNextDay = n => {
+//     showDay((currentDay += n));
+//   };
 
-  const prev = document.getElementById("prev");
-  const next = document.getElementById("next");
+//   const prev = document.getElementById("prev");
+//   const next = document.getElementById("next");
 
-  prev.addEventListener("click", () => {
-    getNextDay(-1);
-  });
+//   prev.addEventListener("click", () => {
+//     getNextDay(-1);
+//   });
 
-  next.addEventListener("click", () => {
-    getNextDay(1);
-  });
+//   next.addEventListener("click", () => {
+//     getNextDay(1);
+//   });
 
-  const defineCurrentDay = n => {
-    showDay((currentDay = n));
-  };
+//   const defineCurrentDay = n => {
+//     showDay((currentDay = n));
+//   };
 
-  quickNavigation.addEventListener("click", e => {
-    for (let i = 0; i < quickNavigation.children.length; i++) {
-      if (quickNavigation.children[i] === e.target) {
-        defineCurrentDay(i);
-      }
-    }
-  });
+//   quickNavigation.addEventListener("click", e => {
+//     for (let i = 0; i < quickNavigation.children.length; i++) {
+//       if (quickNavigation.children[i] === e.target) {
+//         defineCurrentDay(i);
+//       }
+//     }
+//   });
 
-  const showNotification = () => {
-    notificationContainer.style.display = "flex";
-    bigPicture.style.display = "none";
-    mainDescription.children[0].style.display = "block";
-    mainDescription.children[1].style.display = "none";
-  };
+//   const showNotification = () => {
+//     notificationContainer.style.display = "flex";
+//     bigPicture.style.display = "none";
+//     mainDescription.children[0].style.display = "block";
+//     mainDescription.children[1].style.display = "none";
+//   };
 
-  const checkKeyPress = () => {
-    if (event.code === "ArrowLeft") {
-      getNextDay(-1);
-    }
+//   const checkKeyPress = () => {
+//     if (event.code === "ArrowLeft") {
+//       getNextDay(-1);
+//     }
 
-    if (event.code === "ArrowRight") {
-      getNextDay(+1);
-    }
+//     if (event.code === "ArrowRight") {
+//       getNextDay(+1);
+//     }
 
-    if (event.code === "Escape") {
-      close(true);
-    }
+//     if (event.code === "Escape") {
+//       close(true);
+//     }
 
-    if (event.code === "Enter") {
-      close();
-    }
-  };
+//     if (event.code === "Enter") {
+//       close();
+//     }
+//   };
 
-  window.document.addEventListener("keydown", () => {
-    if (notificationContainer.style.display === "flex") {
-      checkKeyPress();
-    } else {
-      showNotification();
-    }
-  });
+//   window.document.addEventListener("keydown", () => {
+//     if (notificationContainer.style.display === "flex") {
+//       checkKeyPress();
+//     } else {
+//       showNotification();
+//     }
+//   });
 
-  bigPicture.addEventListener("click", () => {
-    showNotification();
-  });
-}, 2000);
+//   bigPicture.addEventListener("click", () => {
+//     showNotification();
+//   });
+// }, 2000);
